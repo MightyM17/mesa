b7dc39ca26a7e1b48236f107e1c4f4b5a347791e
Mighty <70426623+MightyM17@users.noreply.github.com>
2022-04-04 00:12:11 +0530

    Update egl_dri2.c
    Test, as we are on version 8

--- src/egl/drivers/dri2/egl_dri2.c
+++ src/egl/drivers/dri2/egl_dri2.c
@@ -2751,8 +2751,7 @@ dri2_query_dma_buf_formats(_EGLDisplay *disp, EGLint max,
    if (max < 0 || (max > 0 && formats == NULL))
       return _eglError(EGL_BAD_PARAMETER, "invalid value for max count of formats");
 
-   if (dri2_dpy->image->base.version < 15 ||
-       dri2_dpy->image->queryDmaBufFormats == NULL)
+   if (dri2_dpy->image->queryDmaBufFormats == NULL)
       return EGL_FALSE;
 
    if (!dri2_dpy->image->queryDmaBufFormats(dri2_dpy->dri_screen, max,
